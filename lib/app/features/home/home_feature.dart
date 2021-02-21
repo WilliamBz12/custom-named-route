23ff04cb4b962be42d4b6a321296c5867c6df056
williambz12 <williamsilva754@gmail.com>
2021-02-21 15:04:41 -0300

    correction in sub features

--- lib/app/features/home/home_feature.dart
+++ lib/app/features/home/home_feature.dart
@@ -1,6 +1,6 @@
 import 'package:custom_app_router/custom_app_router.dart';
 import 'package:structuremodel/app/features/home/home_provider.dart';
-import 'package:structuremodel/app/features/home/subfeatures/details/details_feature.dart';
+import 'package:structuremodel/app/features/home/sub_features/details/details_sub_feature.dart';
 
 import 'pages/home_page.dart';
 
--- lib/app/features/home/sub_features/details/details_sub_feature.dart
+++ lib/app/features/home/sub_features/details/details_sub_feature.dart
@@ -1,6 +1,6 @@
 import 'package:custom_app_router/custom_app_router.dart';
-import 'package:structuremodel/app/features/home/subfeatures/details/pages/details_page.dart';
-import 'package:structuremodel/app/features/home/subfeatures/details/pages/product_page.dart';
+import 'package:structuremodel/app/features/home/sub_features/details/pages/details_page.dart';
+import 'package:structuremodel/app/features/home/sub_features/details/pages/product_page.dart';
 
 class DetailsFeature extends CustomSubFeature {
   @override
